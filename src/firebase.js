fe972aae55eea416cfbc7e6530563324f35c3bcc
titxs <titusnmacharia@gmail.com>
2026-04-03 06:16:56 +1100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCE2inSOEE-dWIkNOND_hBGfkYeZbgopDw",
@@ -12,5 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-
+export const storage = getStorage(app);
